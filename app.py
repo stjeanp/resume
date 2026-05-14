cd02f4d7831608d2f780a0aaaa42f008c4f60ce9
stjeanp <stjeanp@pat-st-jean.com>
2026-05-14 07:28:07 -0500

    Minor tweaks

--- app.py
+++ app.py
@@ -3,10 +3,12 @@
 from resume_app import create_app
 
 
-def main():
-    """ Entrypoint """
-    app = create_app()
-    return app
+app = create_app()
 
-if __name__ == "__main__":
-    main()
+#def main():
+#    """ Entrypoint """
+#    app = create_app()
+#    return app
+
+#if __name__ == "__main__":
+#    main()
