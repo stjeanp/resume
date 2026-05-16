fd661ead5d562e432c49f7cd17830ed26b32772a
Patrick St. Jean <stjeanp@pat-st-jean.com>
2026-05-16 13:54:28 -0500

    Added is_initialized method, cleaned up error handlers

--- resume_app/helpers/resume.py
+++ resume_app/helpers/resume.py
@@ -7,11 +7,14 @@ import logging
 import os
 from typing import Optional
 
+from json.decoder import JSONDecodeError
 from jsonschema import validate
-from jsonschema.exceptions import ValidationError, SchemaError
+from jsonschema.exceptions import ValidationError, SchemaError, _WrappedReferencingError
 
 
 class Resume:
+    SCHEMA_PATH = "schemas/resume.schema.json"
+
     def __init__(self, src: Optional[str] = None) -> None:
         """
         Initialize a resume class from the JSON data in the file passed to us
@@ -33,6 +36,16 @@ class Resume:
 
         self.initialize(src)
 
+    @property
+    def is_initialized(self) -> bool:
+        """
+        Returns the instance's initialized state
+
+        :return: Whether this instance has been initialized yet
+        :rtype: bool
+        """
+        return self.__initialized
+
     def initialize(self, src: str) -> None:
         """
         Initialize the Resume object
@@ -50,34 +63,42 @@ class Resume:
         """
         logger = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
 
+        if self.is_initialized:
+            logger.warning("initialize called on an already initialized instance")
+            return
+
+        msg = "Invalid resume data path supplied"
         if not src:
-            logger.error("Invalid resume data path supplied")
-            raise ValueError("Invalid value for resume path")
+            logger.error(msg)
+            raise ValueError(msg)
 
         if not isinstance(src, str):
-            logger.error("Invalid resume data path supplied")
-            raise TypeError("Invalid resume path supplied")
+            logger.error(msg)
+            raise TypeError(msg)
 
         if not os.path.exists(src):
-            logger.error("Invalid resume data path supplied")
-            raise FileNotFoundError(f"Could not find {src}")
+            logger.error(msg)
+            raise FileNotFoundError(msg)
 
-        with open("schemas/resume.schema.json", "r") as resume_schema:
+        with open(self.SCHEMA_PATH, "r") as resume_schema:
             resume_schema = json.load(resume_schema)
 
-        with open(src, "r") as resume_src:
-            resume_data = json.load(resume_src)
-
         try:
+            with open(src, "r") as resume_src:
+                resume_data = json.load(resume_src)
             validate(instance=resume_data, schema=resume_schema)
-        except SchemaError:
-            logger.error("There is something wrong in the schema")
+        except JSONDecodeError:
+            logger.error("The resume data failed JSON decode")
             raise
+        except (SchemaError, _WrappedReferencingError) as the_exception:
+            logger.error("There is something wrong in the schema")
+            raise SchemaError(the_exception)
         except ValidationError:
             logger.error("The resume data failed schema validation")
             raise
 
         self.__data = resume_data
+        self.__initialized = True
 
     @property
     def name(self) -> str:
